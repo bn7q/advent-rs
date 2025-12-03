42f30a4aec5e81f3bfec146b52e9e038c73a48db
Konstantin Shabanov <konstsh@amazon.com>
2025-12-03 10:48:45 +0000

    2025d03

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -8,6 +8,7 @@ pub mod y2025 {
     pub mod d01;
     pub mod d02;
     pub mod d03;
+    pub mod d04;
 }
 
 pub fn get_puzzle(y: u16, d: u16) -> Option<Box<dyn Puzzle>> {
@@ -17,6 +18,7 @@ pub fn get_puzzle(y: u16, d: u16) -> Option<Box<dyn Puzzle>> {
         (2025, 1) => Some(Box::new(y2025::d01::P)),
         (2025, 2) => Some(Box::new(y2025::d02::P)),
         (2025, 3) => Some(Box::new(y2025::d03::P)),
+        (2025, 4) => Some(Box::new(y2025::d04::P)),
         _ => None,
     }
 }
--- src/puzzles/y2025/d03.rs
+++ src/puzzles/y2025/d03.rs
@@ -1,13 +1,63 @@
+use std::{cmp::max, num::ParseIntError};
+
 use crate::puzzle::{Puzzle, PuzzleResult};
 
 pub struct P;
 
 impl Puzzle for P {
-    fn solve1(&self, _input: &str) -> PuzzleResult {
-        return Ok("no solution".to_string());
+    fn solve1(&self, input: &str) -> PuzzleResult {
+        let jotages: Result<Vec<u32>, ParseIntError> = input
+            .lines()
+            .map(|line| {
+                let (hdc, ldc) = line
+                    .chars()
+                    .rev()
+                    .fold((char::MIN, char::MIN), |(c1, c2), c| {
+                        if c >= c1 || c2 == char::MIN {
+                            (c, max(c1, c2))
+                        } else {
+                            (c1, c2)
+                        }
+                    });
+                hdc.to_string() + ldc.to_string().as_str()
+            })
+            .map(|bat_str| bat_str.parse::<u32>())
+            .collect();
+
+        let res: u32 = jotages?.iter().sum();
+
+        return Ok(res.to_string());
     }
 
-    fn solve2(&self, _input: &str) -> PuzzleResult {
-        return Ok("no solution".to_string());
+    fn solve2(&self, input: &str) -> PuzzleResult {
+        let jotages: Result<Vec<u64>, ParseIntError> = input
+            .lines()
+            .map(|line| {
+                let output_chars = line.chars().rev().fold([char::MIN; 12], |mut chars, c| {
+                    if chars[11] == char::MIN {
+                        chars.rotate_right(1);
+                        chars[0] = c;
+                    } else if c >= chars[0] {
+                        let mut temp = c;
+                        for i in 0..12 {
+                            if temp >= chars[i] {
+                                (chars[i], temp) = (temp, chars[i])
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                    chars
+                });
+                output_chars.iter().fold(String::new(), |output, char| {
+                    output + char.to_string().as_str()
+                })
+            })
+            .map(|bat_str| bat_str.parse::<u64>())
+            .collect();
+
+        let res: u64 = jotages?.iter().sum();
+
+        return Ok(res.to_string());
     }
 }
