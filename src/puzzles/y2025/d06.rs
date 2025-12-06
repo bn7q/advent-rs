1553805376a7e320f0f59a6808b6e80e2c5fd432
Konstantin Shabanov <konstsh@amazon.com>
2025-12-06 23:41:55 +0000

    2025d06

--- src/puzzles/y2025/d06.rs
+++ src/puzzles/y2025/d06.rs
@@ -1,13 +1,80 @@
-use crate::puzzle::{Puzzle, PuzzleResult};
+use std::ops::{Add, Mul};
+
+use crate::{
+    puzzle::{Puzzle, PuzzleResult},
+    utils::grid::transpose,
+};
 
 pub struct P;
 
 impl Puzzle for P {
-    fn solve1(&self, _input: &str) -> PuzzleResult {
-        return Ok("no solution".to_string());
+    fn solve1(&self, input: &str) -> PuzzleResult {
+        let lines: Vec<&str> = input.lines().collect();
+        let (operators_line, numbers_lines) = lines.split_last().expect("Expected multiple lines");
+
+        let operators: Vec<u8> = operators_line
+            .split_whitespace()
+            .map(|o| o.as_bytes()[0])
+            .collect();
+        let rows = numbers_lines
+            .iter()
+            .map(|line| {
+                line.split_whitespace()
+                    .map(|element| {
+                        element
+                            .parse()
+                            .expect(&format!("Value is not a number: {element}"))
+                    })
+                    .collect()
+            })
+            .collect();
+
+        let res = calculate(transpose(rows), operators);
+        return Ok(res.to_string());
     }
 
-    fn solve2(&self, _input: &str) -> PuzzleResult {
-        return Ok("no solution".to_string());
+    fn solve2(&self, input: &str) -> PuzzleResult {
+        let lines: Vec<&str> = input.lines().collect();
+
+        let mut numbers = Vec::new();
+        let mut number_arrays = Vec::new();
+        let mut operators = Vec::new();
+
+        for i in (0..lines[0].len()).rev() {
+            let col: Vec<u8> = lines.iter().map(|l| l.as_bytes()[i]).collect();
+
+            if col.iter().all(|c| c == &b' ') {
+                continue;
+            }
+
+            let (&operator, digits) = col.split_last().expect("Expected multiple lines");
+            let number: u64 = str::from_utf8(digits)?.trim().parse()?;
+            numbers.push(number);
+
+            if operator != b' ' {
+                operators.push(operator);
+                number_arrays.push(numbers.clone());
+                numbers.clear();
+            }
+        }
+
+        let res = calculate(number_arrays, operators);
+        return Ok(res.to_string());
     }
+}
+
+fn calculate(number_arrays: Vec<Vec<u64>>, operators: Vec<u8>) -> u64 {
+    number_arrays
+        .iter()
+        .zip(operators)
+        .map(|(numbers, operator)| {
+            let op: fn(u64, u64) -> u64 = match operator {
+                b'+' => u64::add,
+                b'*' => u64::mul,
+                _ => panic!("Unexpected operator: {operator}"),
+            };
+            let res = numbers.iter().copied().reduce(op).expect("Column is empty");
+            res
+        })
+        .sum()
 }
--- src/utils/grid.rs
+++ src/utils/grid.rs
@@ -160,6 +160,23 @@ impl<T: Copy> Grid<T> {
     }
 }
 
+pub fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| {
+                    n.next()
+                        .expect("2D array is corrupted, unable to transpose")
+                })
+                .collect::<Vec<T>>()
+        })
+        .collect()
+}
+
 #[derive(Debug, Eq, PartialEq)]
 pub enum Error {
     IndicesOutOfBounds(Point),
